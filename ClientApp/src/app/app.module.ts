80df5a4adf13bbf6d9d9536355c73e5205967b91
Zach Ayers <ayers.dev@icloud.com>
2020-10-20 17:19:53 -0500

    retools client app;

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -1,54 +1,36 @@
-import { PaginationComponent } from './components/shared/pagination.component';
-import { VehicleListComponent } from './components/vehicle-list/vehicle-list';
-import * as Raven from 'raven-js'; 
-import { FormsModule } from '@angular/forms'; 
-import { NgModule, ErrorHandler } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
-import { ToastyModule } from 'ng2-toasty';
-import { UniversalModule } from 'angular2-universal';
+import { VehicleService } from '../services/vehicle.service';
 
-import { AppComponent } from './components/app/app.component'
-import { AppErrorHandler } from './app.error-handler';
-import { VehicleService } from './services/vehicle.service';
-import { NavMenuComponent } from './components/navmenu/navmenu.component';
-import { HomeComponent } from './components/home/home.component';
-import { FetchDataComponent } from './components/fetchdata/fetchdata.component';
-import { CounterComponent } from './components/counter/counter.component';
-import { VehicleFormComponent } from './components/vehicle-form/vehicle-form.component';
-
-Raven.config('https://d37bba0c459b46e0857e6e2b3aeff09b@sentry.io/155312').install();
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { NavMenuComponent } from './nav-menu/nav-menu.component';
+import { VehicleFormComponent } from './vehicle-form/vehicle-form.component';
 
 @NgModule({
-    bootstrap: [ AppComponent ],
-    declarations: [
-        AppComponent,
-        NavMenuComponent,
-        CounterComponent,
-        FetchDataComponent,
-        HomeComponent,
-        VehicleFormComponent,
-        VehicleListComponent,
-        PaginationComponent,
-    ],
-    imports: [
-        FormsModule,
-        ToastyModule.forRoot(),
-        UniversalModule, // Must be first import. This automatically imports BrowserModule, HttpModule, and JsonpModule too.
-        RouterModule.forRoot([
-            { path: '', redirectTo: 'vehicles', pathMatch: 'full' },
-            { path: 'vehicles/new', component: VehicleFormComponent },
-            { path: 'vehicles/:id', component: VehicleFormComponent },
-            { path: 'vehicles', component: VehicleListComponent },
-            { path: 'home', component: HomeComponent },
-            { path: 'counter', component: CounterComponent },
-            { path: 'fetch-data', component: FetchDataComponent },
-            { path: '**', redirectTo: 'home' }
-        ])
-    ],
-    providers: [
-      { provide: ErrorHandler, useClass: AppErrorHandler },
-      VehicleService
-    ]
+  declarations: [
+    AppComponent,
+    NavMenuComponent,
+    HomeComponent,
+    VehicleFormComponent,
+  ],
+  imports: [
+    BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
+    HttpClientModule,
+    FormsModule,
+    RouterModule.forRoot([
+      { path: '', component: HomeComponent, pathMatch: 'full' },
+      {
+        path: 'vehicles/new',
+        component: VehicleFormComponent,
+        pathMatch: 'full',
+      },
+    ]),
+  ],
+  providers: [VehicleService],
+  bootstrap: [AppComponent],
 })
-export class AppModule {
-}
+export class AppModule {}
